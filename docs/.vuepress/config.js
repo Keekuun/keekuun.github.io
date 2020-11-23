801113ecc0403343bac7ea97f2a2f201e4b426a9
Jeek <zkkysqs@gmail.com>
2020-11-23 10:28:43 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const headConfig = require('./config/headConfig.js');
 const themeConfig = require('./config/themeConfig');
 module.exports = {
     // 为了部署在gitee
-    base: '/blog/',
+    base: '/blog',
     title: '月上秦少',
     description: '每一个不曾起舞的日子，都是对生命的辜负！',
     locales: {
