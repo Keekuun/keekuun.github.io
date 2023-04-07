d1cb6b8300dbd8f23349b7825a772dd0dcdd2591
zhoukekun <zhoukekun@xunlei.com>
2023-04-07 09:16:48 +0800

    feat: update nav

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -66,11 +66,11 @@ module.exports = {
     // },
     lastUpdated: '上次更新',
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'https://gitee.com/keekuun/blog',
+    // repo: 'https://gitee.com/keekuun/blog',
     // 以下为可选的编辑链接选项
     // 假如文档不是放在仓库的根目录下：
     docsDir: 'docs',
     // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    editLinkText: '在 Gitee 上编辑此页',
+    editLinks: false,
+    // editLinkText: '在 Gitee 上编辑此页',
 };
