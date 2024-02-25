f98ca282c649c8c99dfbdccbbb34cf09ac41b39b
keekuun <zkkysqs@qq.com>
2024-02-25 12:06:42 +0800

    feat: add test sort.mjs && sort

---
+++ test/tool.mjs
@@ -0,0 +1,22 @@
+export function genRandomNumArr(n = 10) {
+  return new Array(n).fill(0).map(d => {
+    let randomNum = ~~(Math.random() * 100)
+    return randomNum
+  })
+}
+
+export function testFunc(func, times = 10) {
+  console.log(`---------------【test ${func.name}】--------------`)
+  for(let i=0; i<times; i++) {
+    console.log(`**************【test ${i+1}】**************`)
+    const arr = genRandomNumArr(10)
+    const nativeSorted = [...arr].sort((a, b) => a - b)
+    console.log(arr.toString())
+    func(arr)
+    console.log('nativeSorted:', nativeSorted.toString())
+    console.log('currentSorted:', arr.toString())
+    console.log(`【结果】${arr.toString() === nativeSorted.toString()}`)
+    console.log(`**************【test ${i+1}】**************`)
+    console.log(' ')
+  }
+}
