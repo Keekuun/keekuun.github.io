352ac8f64f8d32bcdb8f6eb99e7a9b88caa77786
jeek <zkkysqs@gmail.com>
2024-04-24 16:03:04 +0800

    fix: fix git email

---
+++ git_rename.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+OLD_EMAIL="zkkysqs@gamil.com"
+CORRECT_NAME="Jeek"
+CORRECT_EMAIL="zkkysqs@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
