3f14f0028a382a60b7163f3c21ffcf6fed73585e
jeek <zkkysqs@gmail.com>
2025-07-17 21:14:33 +0800

    feat: 添加WebP图片优化插件并更新图片处理规则

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 const pluginConfig = require('./config/pluginConfig.js');
 const headConfig = require('./config/headConfig.js');
 const themeConfig = require('./config/themeConfig');
+
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 module.exports = {
     title: '前端Jeek',
     // gitee pages必须加上仓库名称
@@ -25,4 +27,40 @@ module.exports = {
     },
     // 插件
     plugins: pluginConfig,
+
+    chainWebpack: (config) => {
+        // 图片处理规则
+        config.module
+            .rule('images')
+            .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+            .use('url-loader')
+            .loader('url-loader')
+            .options({
+                limit: 4096,
+                name: 'assets/img/[name].[hash:8].[ext]',
+                esModule: false // 解决Vue模板中的图片引用问题
+            });
+
+        // 生产环境WebP优化
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('imagemin-webp')
+                .use(ImageminWebpWebpackPlugin, [{
+                    config: [{
+                        test: /\.(jpe?g|png)$/,
+                        options: {
+                            quality: 75
+                        }
+                    }],
+                    overrideExtension: true,
+                    detailedLogs: true
+                }]);
+        }
+    },
+
+    // 确保静态资源正确引用
+    configureWebpack: {
+        output: {
+            publicPath: '/'
+        }
+    }
 };
